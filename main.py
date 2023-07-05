814c9ce60c3111132a3f75a4ad499e0777777778
Caesar <Caesar.Zhao@ikotek.com>
2023-07-05 17:53:16 +0800

    优化翻译中程序会卡死；添加用线程翻译功能

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #encoding:UTF-8
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6.QtGui import QShortcut, QKeySequence
+from PyQt6.QtCore import QThread, pyqtSignal
 from gpt_translate import Translate, Setting
 from Ui_translate import Ui_MainWindow
 import sys
@@ -11,7 +12,24 @@ LANGUAGE_DICT = {
     "English":"English"
 }
 
-class Translate_window(QMainWindow, Ui_MainWindow, Translate):
+class Translate_thread(QThread, Translate):
+    translate_signal = pyqtSignal(str)
+
+    def __init__(self):
+        super().__init__()
+
+    def translate_init(self, api_key, text, target_language, gpt_mode):
+        """设置openai key和翻译参数"""
+        openai.api_key = api_key
+        self.text = text
+        self.target_language = target_language
+        self.gpt_mode = gpt_mode
+
+    def run(self):
+        output = self.translate_text(self.text, self.target_language, self.gpt_mode)
+        self.translate_signal.emit(output)
+
+class Translate_window(QMainWindow, Ui_MainWindow):
     def __init__(self) -> None:
         # 初始化
         super().__init__()
@@ -38,12 +56,17 @@ class Translate_window(QMainWindow, Ui_MainWindow, Translate):
         self.setting_toolbar.triggered.connect(self.switch_to_setting_page)
         # API-KEY显示按键
         self.api_key_echo_switch.clicked.connect(self.api_key_show)
+        # 翻译信号
+        self.translate_thread = Translate_thread()
+        self.translate_thread.translate_signal.connect(self.display_translate)
         # # 设置API
         # self.api_key_lineEdit.textChanged.connect(lambda:self.setting.set_config_apikey(self.api_key_lineEdit.text()))
         # # 设置GPT模型
         # self.gpt_model_comboBox.textActivated[str].connect(lambda:self.setting.set_config_gpt_model(self.gpt_model_comboBox.currentText()))
 
     def application_translate_init(self):
+        """初始化一次程序设置"""
+
         # 初始化翻译目标语言
         language_entry = LANGUAGE_DICT.get(self.target_language_comboBox.currentText())
         self.target_language = language_entry
@@ -58,7 +81,7 @@ class Translate_window(QMainWindow, Ui_MainWindow, Translate):
         
         # 初始化api-key
         self.api_key_lineEdit.setText(self.setting.get_config_apikey())
-        openai.api_key = self.setting.get_config_apikey()
+        self.api_key = self.setting.get_config_apikey()
 
         # 初始化代理
         api_proxy = self.setting.get_config_proxy()
@@ -82,8 +105,18 @@ class Translate_window(QMainWindow, Ui_MainWindow, Translate):
         self.original_text.clear()
     
     def start_translate(self):
-        text = self.translate_text(self.original_text.toPlainText(),self.target_language,self.gpt_model)
+        self.translate_thread.translate_init(self.api_key, self.original_text.toPlainText(),
+                                  self.target_language, self.gpt_model)
+        self.translate_thread.start()
+        self.original_text.setToolTip("正在翻译")
+        # 翻译时禁止用户继续写入
+        self.original_text.setReadOnly(True)
+
+    def display_translate(self, text):
         self.target_text.setPlainText(text)
+        self.original_text.setToolTip("Ctrl+T可以快捷翻译")
+        # 翻译完成后取消用户写入限制
+        self.original_text.setReadOnly(False)
 
     def setting_confirm(self):
         self.setting.set_config_gpt_model(self.gpt_model_comboBox.currentText())
